170a8aea995903349c1948a28b32251fb55cd941
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2023-12-09 18:35:14 +0100

    Es 03 done

--- lab01/03-impl-complexnum/ComplexNum.java
+++ lab01/03-impl-complexnum/ComplexNum.java
@@ -2,11 +2,15 @@ class ComplexNum {
     /*
      * Inserire qui la dichiarazione dei campi della classe
      */
+    double re;
+    double im;
 
     void build(double re, double im) {
         /*
          * Inserire qui l'inizializzazione dei campi con i parametri forniti in input
          */
+        this.re = re;
+        this.im = im;
     }
 
     boolean equal(ComplexNum num) {
@@ -14,7 +18,7 @@ class ComplexNum {
          * Implementare il metodo in modo che restituisca true se e solo se il
          * numero complesso è uguale al parametro num passato in input
          */
-        return false;
+        return this.re == num.re && this.im == num.im;
     }
 
     void add(ComplexNum num) {
@@ -22,6 +26,8 @@ class ComplexNum {
          * Implementare il metodo in modo che venga aggiunto il numero complesso
          * passato in input - Rif. Appendice A1 (slide)
          */
+        this.re += num.re;
+        this.im += num.im;
     }
 
     String toStringRep() {
@@ -29,6 +35,6 @@ class ComplexNum {
          * Implementare il metodo in modo che restituisca una rappresentazione
          * testuale del numero complesso
          */
-        return "";
+        return re + "+" + im + "i";
     }
 }
--- lab01/03-impl-complexnum/TestComplexNum.java
+++ lab01/03-impl-complexnum/TestComplexNum.java
@@ -30,5 +30,33 @@ class TestComplexNum {
        *
        * 11) Controllare se l'output corrisponde a quanto preventivato
        */
+      ComplexNum c1 = new ComplexNum();
+      c1.build(3, 5);
+
+      ComplexNum c2 = new ComplexNum();
+      c2.build(7, -4);
+
+      ComplexNum c3 = new ComplexNum();
+      c3.build(-2, 3);
+
+      ComplexNum c4 = new ComplexNum();
+      c4.build(-2, 3);
+
+      System.out.println(c1.toStringRep());
+      System.out.println(c2.toStringRep());
+      System.out.println(c3.toStringRep());
+      System.out.println(c4.toStringRep());
+
+      c1.add(c2);
+      c2.add(c4);
+
+      System.out.println(c1.toStringRep());
+      System.out.println(c2.toStringRep());
+      System.out.println(c3.toStringRep());
+      System.out.println(c4.toStringRep());
+
+      System.out.println(c3.equal(c1));
+      System.out.println(c3.equal(c2));
+      System.out.println(c3.equal(c4));
     }
 }
