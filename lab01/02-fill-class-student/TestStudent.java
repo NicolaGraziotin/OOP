aa37a66be5bc8425ea52de04b511541efa073391
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2023-12-09 18:20:22 +0100

    Es 01 done
    Es 02 done

--- lab01/02-fill-class-student/TestStudent.java
+++ lab01/02-fill-class-student/TestStudent.java
@@ -1,8 +1,16 @@
 class TestStudent {
 
     public static void main(String[] args) {
-        Student someone = new Student();
-        someone.build();
-        someone.printStudentInfo();
+        Student alex = new Student();
+        alex.build("Alex", "Balducci", 1015, 2019);
+        alex.printStudentInfo();
+
+        Student angel = new Student();
+        angel.build("Angel", "Bianchi", 1016, 2018);
+        angel.printStudentInfo();
+
+        Student andrea = new Student();
+        andrea.build("Andrea", "Bracci", 1017, 2017);
+        andrea.printStudentInfo();
     }
 }
