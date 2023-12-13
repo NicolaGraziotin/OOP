7faaf3f8636e4fca52323dd842f7cf5bbe602e31
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2023-12-13 10:46:30 +0100

    Es 62 done

--- lab06/62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ lab06/62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,7 +1,11 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
@@ -21,19 +25,34 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 1000; i < 2000; i++) {
+            arrayList.add(i);
+        }
+
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        List<Integer> linkedList = new LinkedList<>(arrayList);
+
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        Integer tmp = arrayList.get(0);
+        arrayList.set(0, arrayList.get(arrayList.size() - 1));
+        arrayList.set(arrayList.size() - 1, tmp);
+
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        for (var elem : arrayList) {
+            System.out.println(elem.intValue());
+        }
+
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +60,44 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        long time = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            arrayList.add(0, i);
+        }
+        time = System.nanoTime() - time;
+        var millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("array list: " + millis);
+
+        time = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            linkedList.add(0, i);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("linked list: " + millis);
+
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+        time = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            arrayList.get(arrayList.size()/2);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("array middle: " + millis);
+
+        time = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            linkedList.get(linkedList.size()/2);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("linked middle: " + millis);
+
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -63,8 +114,21 @@ public final class UseListsAndMaps {
          *
          * Oceania -> 38,304,000
          */
+        final Map<String, Long> population = new HashMap<>();
+        population.put("Africa", 1110635000L);
+        population.put("Americas", 972005000L);
+        population.put("Antartica", 0L);
+        population.put("Asia", 4298723000L);
+        population.put("Europe", 742452000L);
+        population.put("Oceania", 38304000L);
+
         /*
          * 8) Compute the population of the world
          */
+        long totPopulation = 0;
+        for (var elem : population.entrySet()) {
+            totPopulation += elem.getValue();
+        }
+        System.out.println("tot population: " + totPopulation);
     }
 }
